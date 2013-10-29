feeab0d3a7f771b60d8e7ee62d3ed5f6cfae0750
forsytbe <forsythebe@gmail.com>
2013-10-28 19:24:30 -0700

    No real changes

--- src/test/example/helloworld/MainActivity.java
+++ src/test/example/helloworld/MainActivity.java
@@ -115,6 +115,7 @@ public class MainActivity extends Activity {
     			
     				
     				writeCommsToFile();
+    				
     				writeMpgData(msg.getData().getBoolean("writeTime"));
     			
     			
